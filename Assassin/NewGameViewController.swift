239dc133bfeb78e29b0f17859dca9f1af7862833
quanvo87 <quanvo@viisi.cs.utexas.edu>
2015-10-19 18:10:10 -0500

    trying to populate friends picker to invite them

--- Assassin/NewGameViewController.swift
+++ Assassin/NewGameViewController.swift
@@ -6,19 +6,54 @@
 //  Copyright © 2015 Courtney Bohrer. All rights reserved.
 //
 
-import UIKit
-
 class NewGameViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var gameNameTextField: UITextField!
+    @IBOutlet weak var inviteFriendsTableView: UITableView!
+    
     var invitedPlayers:[PFUser] = []
+    var inviteFriendsTableViewData:[NSDictionary] = []
+    
+    var friendNames:[NSString] = []
+    var friendIDs:[NSString] = []
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
         gameNameTextField.delegate = self
-    }
+        
+        // pull current user's friends to populate invite friends table view
+        let fbRequest = FBSDKGraphRequest(graphPath:"/me/friends", parameters: nil)
+        fbRequest.startWithCompletionHandler { (connection : FBSDKGraphRequestConnection!, result : AnyObject!, error : NSError!) -> Void in
+            
+            if error == nil {
+                
+                print("Friends are : \(result)")
+                
+                if let friendObjects = result["data"] as? [NSDictionary] {
+                    
+                    self.inviteFriendsTableViewData = friendObjects
+                    
+                    for friendObject in friendObjects {
+                        
+                        // self.inviteFriendsTableViewData.append(friendObject["id"] as! String)
+                        
+                        /*
+                        print(friendObject["id"] as! NSString)
+                        print(friendObject["name"]as! NSString)
+                        */
 
+                        self.friendNames.append(friendObject["name"] as! NSString)
+                        self.friendIDs.append(friendObject["id"] as! NSString)
+                    }
+                }
+            } else {
+                print("Error Getting Friends \(error)");
+            }
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
@@ -31,18 +66,34 @@ class NewGameViewController: UIViewController, UITextFieldDelegate {
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         view.endEditing(true)
     }
-
-    @IBAction func createGameAction(sender: AnyObject) {
-        
-        let game:Game = Game(gameName: gameNameTextField.text!, invitedPlayers: [], activePlayers: [])
-     
-        game.activePlayers.append(PFUser.currentUser()!)
-        game.setObject(invitedPlayers, forKey: "invitedPlayers")
-        game.setObject(game.activePlayers, forKey: "activePlayers")
-        game.setValue(gameNameTextField.text!, forKey: "Name")
-        
-        game.saveInBackground()
-        
+    
+    // populate invite friends table view
+    
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return self.friendNames.count
     }
     
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        
+        let cell:UITableViewCell = UITableViewCell(style:UITableViewCellStyle.Default, reuseIdentifier:"cell")
+        
+        print(friendNames[0])
+        
+        cell.textLabel?.text = friendNames[indexPath.row] as String
+        
+        return cell
+    }
+    
+    // create game
+    
+    @IBAction func createGameAction(sender: AnyObject) {
+        
+        let game:Game = Game(gameName: gameNameTextField.text!, invitedPlayers: [], activePlayers: [PFUser.currentUser()!])
+        
+        game.setValue(gameNameTextField.text!, forKey: "Name")
+        game.setObject(invitedPlayers, forKey: "invitedPlayers")
+        game.setObject(game.activePlayers, forKey: "activePlayers")
+        
+        game.saveInBackground()
+    }
 }
--- Assassin/Player.swift
+++ Assassin/Player.swift
@@ -6,33 +6,33 @@
 //  Copyright © 2015 Courtney Bohrer. All rights reserved.
 //
 
-
-class Player: PFUser{
-    var firstName:String;
-    var lastName:String;
-    var friends:String;
-    var invites:[String];
-    var currentGames:[String];
-
+class Player: PFUser {
+    
+    var firstName:String
+    var lastName:String
+    var friends:String
+    var invites:[String]
+    var currentGames:[String]
+    
     override class func initialize() {
         struct Static {
-            static var onceToken : dispatch_once_t = 0;
+            static var onceToken : dispatch_once_t = 0
         }
         dispatch_once(&Static.onceToken) {
             self.registerSubclass()
         }
     }
     
-    
     override init(){
-        firstName = "";
-        lastName = "";
-        friends = "";
-        invites = [];
-        currentGames = [];
+        
+        firstName = ""
+        lastName = ""
+        friends = ""
+        invites = []
+        currentGames = []
         
         super.init()
+        
     }
-    
     
 }
