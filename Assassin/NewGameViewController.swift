be119f886ae32f2271c4984fdf1e929620ac9375
quanvo87 <quanvo@tano.cs.utexas.edu>
2015-10-19 21:52:33 -0500

    populate friend picker table view working

--- Assassin/NewGameViewController.swift
+++ Assassin/NewGameViewController.swift
@@ -6,16 +6,14 @@
 //  Copyright © 2015 Courtney Bohrer. All rights reserved.
 //
 
-class NewGameViewController: UIViewController, UITextFieldDelegate {
+class NewGameViewController: UIViewController, UITextFieldDelegate, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var gameNameTextField: UITextField!
-    @IBOutlet weak var inviteFriendsTableView: UITableView!
+    @IBOutlet weak var friendPickerTableView: UITableView!
     
-    var invitedPlayers:[PFUser] = []
-    var inviteFriendsTableViewData:[NSDictionary] = []
+    private var invitedPlayers:[PFUser] = []
     
-    var friendNames:[NSString] = []
-    var friendIDs:[NSString] = []
+    private var friends = PFUser.currentUser()!.objectForKey("Friends") as! NSArray
     
     override func viewDidLoad() {
         
@@ -23,69 +21,33 @@ class NewGameViewController: UIViewController, UITextFieldDelegate {
         
         gameNameTextField.delegate = self
         
-        // pull current user's friends to populate invite friends table view
-        let fbRequest = FBSDKGraphRequest(graphPath:"/me/friends", parameters: nil)
-        fbRequest.startWithCompletionHandler { (connection : FBSDKGraphRequestConnection!, result : AnyObject!, error : NSError!) -> Void in
-            
-            if error == nil {
-                
-                print("Friends are : \(result)")
-                
-                if let friendObjects = result["data"] as? [NSDictionary] {
-                    
-                    self.inviteFriendsTableViewData = friendObjects
-                    
-                    for friendObject in friendObjects {
-                        
-                        // self.inviteFriendsTableViewData.append(friendObject["id"] as! String)
-                        
-                        /*
-                        print(friendObject["id"] as! NSString)
-                        print(friendObject["name"]as! NSString)
-                        */
-
-                        self.friendNames.append(friendObject["name"] as! NSString)
-                        self.friendIDs.append(friendObject["id"] as! NSString)
-                    }
-                }
-            } else {
-                print("Error Getting Friends \(error)");
-            }
-        }
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-    
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
-    
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        view.endEditing(true)
-    }
-    
-    // populate invite friends table view
-    
-    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
-        return self.friendNames.count
-    }
-    
-    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        self.friendPickerTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
         
-        let cell:UITableViewCell = UITableViewCell(style:UITableViewCellStyle.Default, reuseIdentifier:"cell")
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.friends.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        print(friendNames[0])
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+        let friend = self.friends[indexPath.row]
         
-        cell.textLabel?.text = friendNames[indexPath.row] as String
+        cell.textLabel!.text = friend.objectForKey("name")! as? String
         
         return cell
     }
     
-    // create game
-    
+    /*
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        let index:Int = indexPath.row
+        let friend:PFUser =
+        
+    }
+    */
+
     @IBAction func createGameAction(sender: AnyObject) {
         
         let game:Game = Game(gameName: gameNameTextField.text!, invitedPlayers: [], activePlayers: [PFUser.currentUser()!])
@@ -95,5 +57,20 @@ class NewGameViewController: UIViewController, UITextFieldDelegate {
         game.setObject(game.activePlayers, forKey: "activePlayers")
         
         game.saveInBackground()
+        
     }
+
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
 }
