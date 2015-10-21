e519d453f50f54e0a0de58c0519b5fd4bace6583
quanvo87 <quanvo@tano.cs.utexas.edu>
2015-10-20 22:41:44 -0500

    starting sending everyone invites

--- Assassin/NewGameViewController.swift
+++ Assassin/NewGameViewController.swift
@@ -12,7 +12,7 @@ class NewGameViewController: UIViewController, UITextFieldDelegate, UITableViewD
     @IBOutlet weak var friendPickerTableView: UITableView!
     
     private let game:Game = Game()
-    private let friends = PFUser.currentUser()!.objectForKey("Friends") as! NSArray
+    private let friends:NSArray = PFUser.currentUser()!.objectForKey("Friends") as! NSArray
     
     override func viewDidLoad() {
         
@@ -21,9 +21,9 @@ class NewGameViewController: UIViewController, UITextFieldDelegate, UITableViewD
         gameNameTextField.delegate = self
         
         self.friendPickerTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        
     }
     
+    // populate friend picker table view
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.friends.count
     }
@@ -46,9 +46,9 @@ class NewGameViewController: UIViewController, UITextFieldDelegate, UITableViewD
         if !self.game.invitedPlayers.contains(friendId) {
             self.game.invitedPlayers.append(friendId)
         }
-        
     }
-
+    
+    // create game
     @IBAction func createGameAction(sender: AnyObject) {
         
         // create the game and save
@@ -57,7 +57,47 @@ class NewGameViewController: UIViewController, UITextFieldDelegate, UITableViewD
         self.game.setObject(self.game.invitedPlayers, forKey: "invitedPlayers")
         self.game.setObject(self.game.activePlayers, forKey: "activePlayers")
         
-        self.game.saveInBackground()
+        self.game.saveInBackgroundWithBlock {
+            (success: Bool, error: NSError?) -> Void in
+            if (success) {
+                // The object has been saved.
+                let gameId:String = self.game.objectId!
+                for player in self.game.invitedPlayers{
+                    let query:PFQuery = PFUser.query()!
+                    query.whereKey("FacebookID", equalTo: player)
+                    query.getFirstObjectInBackgroundWithBlock {
+                        (object: PFObject?, error: NSError?) -> Void in
+                        if error != nil || object == nil {
+                            print("The getFirstObject request failed.")
+                        } else {
+                            // The find succeeded.
+                            print("Successfully retrieved the object.")
+                            
+                            // var invitedGames:[String] = object!.objectForKey("invitedGames") as! [String]
+                            // invitedGames.append("dummy arrray")
+                            // object!.setObject(invitedGames, forKey: "invitedGames")
+                            // object["invitedGames"] = invitedGames
+                            // object!.setValue("new courtney", forKey: "Name")
+                            // object!.addUniqueObjectsFromArray(["hi"], forKey: "invitedGames")
+                            // object!.saveInBackground()
+                            
+                            object!.saveInBackgroundWithBlock {
+                                (success: Bool, error: NSError?) -> Void in
+                                if (success) {
+                                    // The object has been saved.
+                                    print("Object saved")
+                                } else {
+                                    // There was a problem, check error.description
+                                    print("Object not saved")
+                                }
+                            }
+                        }
+                    }
+                }
+            } else {
+                // There was a problem, check error.description
+            }
+        }
         
         /*
         
@@ -66,9 +106,9 @@ class NewGameViewController: UIViewController, UITextFieldDelegate, UITableViewD
         
         let currentUser:PFUser = PFUser.currentUser()!
         var currentGames:[String] = currentUser.objectForKey("currentGames") as! [String]
-
+        
         currentGames.append(self.game.objectForKey("objectId") as! String)
-
+        
         currentUser.saveInBackground()
         
         */
@@ -78,9 +118,8 @@ class NewGameViewController: UIViewController, UITextFieldDelegate, UITableViewD
         // i tried grabbing the game object id above, not able to know if that works
         // we need to have the current game controller be some kind of waiting for room till game starts/drops
         // game needs to start when invite list is empty
-        
     }
-
+    
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
@@ -93,5 +132,4 @@ class NewGameViewController: UIViewController, UITextFieldDelegate, UITableViewD
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
 }
